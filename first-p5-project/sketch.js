81022486d018bb7554cd5d665717edd1c56623c4
logavin <35992187+logavin@users.noreply.github.com>
2018-01-31 17:58:50 +0800

    drew shapes

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,6 +1,11 @@
-function setup(){
-
-}
+    function setup() {
+        createCanvas(300, 300);
+        background(225,225,2000000);
+        
+        rect(100,150,100,50);
+        ellipse(100,20,200,40);
+        line(20, 50, 100, 250);
+    } 
 
 function draw(){
 
